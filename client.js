dac4945ac09810ce3a4f4b044d9b225db0c13304
anrahmapuri <88900360+anrahmapuri@users.noreply.github.com>
2023-04-02 23:20:47 +0700

    Update client.js

--- client.js
+++ client.js
@@ -8,8 +8,8 @@ const crudService = protoDescriptor.crud.CrudService;
 const client = new crudService('localhost:50051', grpc.credentials.createInsecure());
 
 // request untuk membuat data  
-function create(name, NRP) {
-  const request = { name, NRP };
+function create(username, password) {
+  const request = { username, password };
   return new Promise((resolve, reject) => {
     client.create(request, (err, response) => {
       if (err) {
@@ -36,8 +36,8 @@ function read(id) {
 }
 
 // request untuk update data  
-function update(id, name, NRP) {
-  const request = { id, name, NRP };
+function update(id, username, password) {
+  const request = { id, username, password };
   return new Promise((resolve, reject) => {
     client.update(request, (err, response) => {
       if (err) {
@@ -63,6 +63,7 @@ function remove(id) {
   });
 }
 
+
 // membuat dua kali input revisi operasi
 function askQuestion(question) {
   return new Promise((resolve) => {
@@ -75,22 +76,43 @@ function askQuestion(question) {
 
 async function main() {
   try {
-
-    // input pertama 
-    const name = await askQuestion('Name: ');
-    const NRP = await askQuestion('NRP: ');
-    const id = await create(name, NRP);
-    console.log('Data berhasil masuk dengan ID :', id);
-    const user = await read(id);
-    console.log('Data :', user);
-
-    // input kedua -> untuk revisi operasi 
-    const newName = await askQuestion('New name: ');
-    const newNRP = await askQuestion('New NRP: ');
-    await update(id, newName, newNRP);
-    console.log('Data berhasil diupdate!');
-    await remove(id);
-    console.log('Data berhasil dihapus!');
+    const choice = await askQuestion('Halo! Selamat datang di Passwod Manager :D \nSilakan pilih fitur di bawah ini \n1. Menambahkan Data \n2. Menampilkan Data \n3. Merevisi Data\n4. Menghapus\nPilihanmu? ');
+    switch (choice) {
+      case '1': {
+        const username = await askQuestion('username: ');
+        const password = await askQuestion('password: ');
+        const id = await create(username, password);
+        console.log(`Data berhasil masuk dengan ID : ${id}`);
+        break;
+      }
+      case '2': {
+        const id = await askQuestion('ID : ');
+        try {
+          const users = await read(id);
+          console.log('Data:', users);
+        } catch (err) {
+          console.error(`Data dengan username : ${username} tidak terdeteksi!`);
+        }
+        break;
+      }
+      case '3': {
+        const id = await askQuestion('ID: ');
+        const ussename = await askQuestion('Masukan username baru : ');
+        const password = await askQuestion('Masukan password baru : ');
+        await update(id, ussename, password);
+        console.log('Data berhasil direvisi!');
+        break;
+      }
+      case '4': {
+        const id = await askQuestion('ID: ');
+        await remove(id);
+        console.log('Data berhasil dihapus!');
+        break;
+      }
+      default: {
+        console.log('ERROR: pilihanmu salah D:');
+      }
+    }
   } catch (err) {
     console.error(err);
   } finally {
